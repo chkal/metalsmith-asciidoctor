902c5178554adf46379f75df061e1c169f5c4a5b
Christian Kaltepoth <christian@kaltepoth.de>
2017-12-20 10:06:03 +0100

    Some test coverage

---
+++ test/test.js
@@ -0,0 +1,28 @@
+const Metalsmith = require("metalsmith");
+const asciidoctor = require("../src/index.js");
+const expect = require("chai").expect;
+const path = require("path");
+
+describe("All tests", () => {
+
+  it("Should render with default options", testDone => {
+
+    const metalsmith = Metalsmith(path.join(__dirname, "fixture"))
+      .use(asciidoctor())
+      .use((files, metalsmith, pluginDone) => {
+
+        // HTML file is created and AsciiDoc file removed
+        expect(files).to.have.property("index.html");
+        expect(files).not.to.have.property("index.adoc");
+
+        // check file contents
+        expect(files["index.html"].contents.toString()).to.have.string("Hello <strong>World</strong>");
+        
+        pluginDone();
+      });
+
+    metalsmith.build(err => testDone(err));
+
+  });
+  
+});
